d24494b4cf27b2d9372659d5c52056877523fa66
elainechan <elaine.chan@outlook.com>
2018-08-10 23:19:02 -0400

    Changes name of handler function.

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -10,7 +10,7 @@ export default class Search extends React.Component {
       searchText: ''
     }
 
-    onChange = (event) => {
+    handleChange = (event) => {
       this.setState({
         searchText: event.target.value
       });
@@ -27,7 +27,7 @@ export default class Search extends React.Component {
       /* {albums} */
       <div className="search-div">
       <form className="search-form" id="search-form" onSubmit={this.handleSubmit.bind(this)}>
-      <input className="search-input" value={this.state.searchText} onChange={this.onChange.bind(this)}
+      <input className="search-input" value={this.state.searchText} onChange={this.handleChange.bind(this)}
       placeholder="Search artist"></input>
       </form>
       <button className="submit" form="search-form" type="submit">SUBMIT</button>
